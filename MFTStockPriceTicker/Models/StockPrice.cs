dc6e73999aac208631a53333d944f6b509a1e605
darter <dar@mir-robots.com>
2024-10-15 15:30:51 +0200

    Add existing frontend project files to Git

---
+++ MFTStockPriceTicker/Models/StockPrice.cs
@@ -0,0 +1,8 @@
+namespace MFTStockPriceTicker.Models
+{
+    public class StockPrice
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ MFTStockPriceTicker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MFTStockPriceTicker.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace MFTStockPriceTicker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5262/stockPriceHub") 
+                .Build();
+            
+            connection.On<List<StockPrice>>("ReceiveStockPrices", stockPrices =>
+            {
+                foreach (var stock in stockPrices)
+                {
+                    Console.WriteLine($"Stock: {stock.Name}, Price: {stock.Price}");
+                }
+            });
+
+            await connection.StartAsync();
+            Console.WriteLine("Connected to the stock price hub. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
